2dff327b58ef5419ad0c55b4d5712c1d92dad5be
realth000 <a598919335@gmail.com>
2022-05-28 21:48:00 +0800

    feat(client): Use kingpin in client.
    Use kingpin in client command line parsing:
    * Not support load config from file yet.

--- go.mod
+++ go.mod
@@ -6,9 +6,12 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.1
 	google.golang.org/grpc v1.46.2
 	google.golang.org/protobuf v1.28.0
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
 
 require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
 	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
--- greeter_client/greeter_client.go
+++ greeter_client/greeter_client.go
@@ -3,91 +3,107 @@ package main
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"flag"
 	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
+	"gopkg.in/alecthomas/kingpin.v2"
 	"io/ioutil"
 	"log"
+	"testgrpc/greeter_client/config"
 )
 
 const (
 	defaultName = "world gRPC!"
 )
 
+// Command line flags.
 var (
-	flagServerUrl      string // Command line flag definition.
-	flagServerPort     uint
-	flagName           string
-	flagDisableSSL     bool
-	flagMutualAuth     bool
-	actionSayHello     bool
-	actionDownloadFile bool
-	argConfigFile      string
-	argDownloadPath    string
-	argSSLCertPath     string
-	argSSLKeyPath      string
-	argSSLCACertPath   string
+	flagConfigFile = kingpin.Flag("config-file", "Config file [*.toml].").String()
+
+	flagServerUrl  = kingpin.Flag("server-ip", "Server IP.").Short('i').String()
+	flagServerPort = kingpin.Flag("server-port", "Server port [0-65535].").Short('p').Uint()
+	flagName       = kingpin.Flag("client-name", "Client name.").Short('n').String()
+
+	flagSSL        = kingpin.Flag("ssl", "Use SSL in connecting with server. Use --no-ssl to disable ssl.").Default("true").Bool()
+	flagMutualAuth = kingpin.Flag("mutual-auth", "Mutual authentication in SSL handshake.").Default("true").Bool()
+	flagSSLCert    = kingpin.Flag("cert", "SSL credential file[*.pem] path.").String()
+	flagSSLKey     = kingpin.Flag("key", "SSL private key file[*.key] path.").String()
+	flagSSLCACert  = kingpin.Flag("ca-cert", "SSL CA credential file[*.pem] path.").String()
+
+	cmdSayHello     = kingpin.Command("say-hello", "Say hello to server, used for testing.")
+	cmdDownloadFile = kingpin.Command("download-file", "Download file from server.")
+	flagFileName    = cmdDownloadFile.Flag("file-name", "Name of file to download.").String()
 )
 
-func init() {
-	flag.StringVar(&flagServerUrl, "u", "", "Server url")
-	flag.UintVar(&flagServerPort, "p", 0, "Server port [0-65535]")
-	flag.StringVar(&flagName, "n", "", "Set client name")
-	flag.BoolVar(&flagDisableSSL, "disablessl", false, "disable ssl(use http instead of https)")
-	flag.BoolVar(&flagMutualAuth, "mutualAuth", true, "use mutual authentication in SSL handshake")
-	flag.BoolVar(&actionSayHello, "sayhello", false, "Say hello to server")
-	flag.BoolVar(&actionDownloadFile, "downloadfile", false, "Download file from server")
-	flag.StringVar(&argConfigFile, "config", "", "load config file[*.toml]")
-	flag.StringVar(&argDownloadPath, "downloadpath", "", "Specify download path")
-	flag.StringVar(&argSSLCertPath, "cert", "", "SSL credential file[*.pem] path")
-	flag.StringVar(&argSSLKeyPath, "key", "", "SSL private key file[*.key] path")
-	flag.StringVar(&argSSLCACertPath, "cacert", "", "CA credential file[*.pem] path")
-	flag.Parse()
-	checkFlag()
+func loadConfigFile() {
+	if *flagConfigFile == "" {
+		return
+	}
+	var cc = config.ClientConfig{}
+	err := config.LoadConfigFile(*flagConfigFile, &cc)
+	if err != nil {
+		log.Fatalf("can not load config file:%v", err)
+	}
+	fmt.Println(cc)
+	//flag.Visit(func(s *flag.Flag) {
+	//	switch s.Name {
+	//	case "u":
+	//		flagServerUrl =
+	//	}
+	//})
 }
 
 func checkFlag() {
-	if flagServerUrl == "" {
-		flagServerUrl = "localhost"
-	}
-	if flagServerPort == 0 {
-		log.Fatalln("Server port not set")
-	} else if flagServerPort > 65535 {
-		log.Fatalf("Invalid port: %d\n", flagServerPort)
-	}
-	if actionDownloadFile && argDownloadPath == "" {
-		log.Fatalf("Download path not set")
-	}
-	if !flagDisableSSL {
-		if argSSLCertPath == "" {
-			log.Fatalf("ssl enabled, but credential file[*.pem] not loaded")
-		}
-		if flagMutualAuth {
-			if argSSLKeyPath == "" {
-				log.Fatalf("ssl enabled, but private key file[*.key] not loaded")
-			}
-			if argSSLCACertPath == "" {
-				log.Fatalf("mutual authentication enabled, but CA credential file[*.pem] not loaded")
-			}
-		}
-	}
+	//if flagServerUrl == "" {
+	//	flagServerUrl = "localhost"
+	//}
+	//if flagServerPort == 0 {
+	//	log.Fatalln("Server port not set")
+	//} else if flagServerPort > 65535 {
+	//	log.Fatalf("Invalid port: %d\n", flagServerPort)
+	//}
+	//if actionDownloadFile && argDownloadPath == "" {
+	//	log.Fatalf("Download path not set")
+	//}
+	//if !flagDisableSSL {
+	//	if argSSLCertPath == "" {
+	//		log.Fatalf("ssl enabled, but credential file[*.pem] not loaded")
+	//	}
+	//	if flagMutualAuth {
+	//		if argSSLKeyPath == "" {
+	//			log.Fatalf("ssl enabled, but private key file[*.key] not loaded")
+	//		}
+	//		if argSSLCACertPath == "" {
+	//			log.Fatalf("mutual authentication enabled, but CA credential file[*.pem] not loaded")
+	//		}
+	//	}
+	//}
 }
 
 func main() {
 	// Setup connection.
+	kingpin.Parse()
+	loadConfigFile()
+	checkFlag()
 	var conn *grpc.ClientConn
-	if !flagDisableSSL {
-		if flagMutualAuth {
+	if !*flagSSL {
+		//conn, err := grpc.Dial(address, grpc.WithInsecure()) // deprecated
+		c, err := grpc.Dial(fmt.Sprintf("%s:%d", *flagServerUrl, *flagServerPort), grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Fatalf("can not dail %s:%d :%v", *flagServerUrl, *flagServerPort, err)
+		}
+		conn = c
+	} else {
+		if *flagMutualAuth {
 			// Mutual authentication.
-			cert, err := tls.LoadX509KeyPair(argSSLCertPath, argSSLKeyPath)
+			cert, err := tls.LoadX509KeyPair(*flagSSLCert, *flagSSLKey)
 			if err != nil {
 				log.Fatalf("can not load SSL credential:%v", err)
 			}
 
 			certPool := x509.NewCertPool()
-			credBytes, err := ioutil.ReadFile(argSSLCACertPath)
+			credBytes, err := ioutil.ReadFile(*flagSSLCACert)
 			if err != nil {
 				log.Fatalf("can not load CA credential:%v", err)
 			}
@@ -113,51 +129,43 @@ func main() {
 					return err
 				},
 			})
-			c, err := grpc.Dial(fmt.Sprintf("%s:%d", flagServerUrl, flagServerPort), grpc.WithTransportCredentials(cred))
+			c, err := grpc.Dial(fmt.Sprintf("%s:%d", *flagServerUrl, *flagServerPort), grpc.WithTransportCredentials(cred))
 			if err != nil {
-				log.Fatalf("can not dail %s:%d :%v", flagServerUrl, flagServerPort, err)
+				log.Fatalf("can not dail %s:%d :%v", *flagServerUrl, *flagServerPort, err)
 			}
 			conn = c
 		} else {
-			cred, err := credentials.NewClientTLSFromFile(argSSLCertPath, "")
+			cred, err := credentials.NewClientTLSFromFile(*flagSSLCert, "")
 			if err != nil {
 				log.Fatalf("error ")
 			}
-			c, err := grpc.Dial(fmt.Sprintf("%s:%d", flagServerUrl, flagServerPort), grpc.WithTransportCredentials(cred))
+			c, err := grpc.Dial(fmt.Sprintf("%s:%d", *flagServerUrl, *flagServerPort), grpc.WithTransportCredentials(cred))
 			if err != nil {
-				log.Fatalf("can not dail %s:%d :%v", flagServerUrl, flagServerPort, err)
+				log.Fatalf("can not dail %s:%d :%v", *flagServerUrl, *flagServerPort, err)
 			}
 			conn = c
 		}
-	} else {
-		//conn, err := grpc.Dial(address, grpc.WithInsecure()) // deprecated
-		c, err := grpc.Dial(fmt.Sprintf("%s:%d", flagServerUrl, flagServerPort), grpc.WithTransportCredentials(insecure.NewCredentials()))
-		if err != nil {
-			log.Fatalf("can not dail %s:%d :%v", flagServerUrl, flagServerPort, err)
-		}
-		conn = c
 	}
 
 	defer conn.Close()
 
 	// Contact the server and print out its response.
 	var name string
-	if flagName == "" {
+	if *flagName == "" {
 		name = defaultName
 	} else {
-		name = flagName
+		name = *flagName
 	}
 
-	if actionSayHello {
+	switch kingpin.Parse() {
+	case "say-hello":
 		r, err := SayHello(conn, name)
 		if err != nil {
 			log.Fatalf("error greeting: %v\n", err)
 		}
 		log.Printf("successful greet: %s", r.Message)
-	}
-
-	if actionDownloadFile {
-		DownloadFile(conn, name, argDownloadPath)
+	case "download-file":
+		DownloadFile(conn, name, *flagFileName)
 		log.Printf("download finish")
 	}
 }
